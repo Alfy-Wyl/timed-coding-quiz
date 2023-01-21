014b7f27fe3e283342fb08cf6233433ff90b5074
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-01-21 16:42:13 +0000

    modified code in questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,31 +1,31 @@
 // Create Variable for questions in an Array of Objects
-const question = [
+let question = [
     {
-        "question": 'Javascript is an _______ language?',
+        "title": 'Javascript is an _______ language?',
         "options": ['Object-Oriented', 'Object-Based', 'Spoken', 'None of the above'],
         "answer": 'Object-Oriented'
     },
 
     {
-        "question": 'Which of the following keywords is used to define a variable in Javascript?',
+        "title": 'Which of the following keywords is used to define a variable in Javascript?',
         "options": ['var', 'let', 'const', 'All of the above'],
         "answer": 'All of the above'
     },
 
     {
-        "question": 'Which of the following methods is used to access HTML elements using Javascript?',
+        "title": 'Which of the following methods is used to access HTML elements using Javascript?',
         "options": ['getElementById()', 'getElementByClassName()', 'Both A and B', 'None of the above' ],
         "answer": 'Both A and B'
     },
 
     {
-        "question": 'Which of the following methods can be used to display data in some form using Javascript?',
+        "title": 'Which of the following methods can be used to display data in some form using Javascript?',
         "options": ['alert', 'console.log', 'document.write', 'All of the above'],
         "answer": 'All of the above'
     },
 
     {
-        "question": 'Which function is used to serialize an object into a JSON string in Javascript?',
+        "title": 'Which function is used to serialize an object into a JSON string in Javascript?',
         "options": ['stringify()', 'parse()', 'convert()', 'object{}' ],
         "answer": 'stringify'
     },
