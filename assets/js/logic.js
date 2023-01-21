e54d1170b797851edba375bcc54904a2027553e9
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-01-21 12:31:24 +0000

    added Javascript starter codes to begin the project

---
+++ assets/js/logic.js
@@ -0,0 +1,68 @@
+// Create Variables to handle status of the quiz
+
+
+
+
+
+
+// Create Selectors using Single Element Selector to target HTML Elements
+
+
+
+
+
+// Create Function to grab the Questions
+function getQuestion(){
+
+}
+
+
+// Create Function to handle Question Click by User
+function questionClick(){
+
+}
+
+
+// Create Function to handle Quiz Start by User
+function startQuiz(){
+
+}
+
+
+// Create Function to handle End Quiz
+function endQuiz(){
+
+}
+
+
+// Create Function to handle Timer Countdown
+function timerCountDown(){
+
+}
+
+
+// Create Function to handle Saving Highscore
+function saveHighscore(){
+
+}
+
+
+// Create Function to check when user enters initials
+function checkForEnter(event){
+
+}
+
+
+// Create an Event Listener to handle the start button when it is clicked
+startButton.addEventListener("click", startQuiz);
+
+
+
+// Create an Event Listener to handle the submit button when it is clicked
+submitButton.addEventListener("click", saveHighscore);
+
+
+
+// Create an Event Listener to handle the user initials input
+initialElement.addEventListener("click", checkForEnter);
+ 
---
+++ assets/js/scores.js
@@ -0,0 +1,20 @@
+// Create Functions to execute printing and clearing highscores
+
+function printHighScores(){
+
+}
+
+function clearHighScores(){
+
+}
+
+
+// Set a Variable for the Clear buttton
+// Add an Event Listener to execute a specific task when the button is clicked
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScores);
+
+
+// Call the function printHighscores
+printHighScores()
