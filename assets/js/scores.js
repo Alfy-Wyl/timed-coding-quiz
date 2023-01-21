f83347984a76a55d1bc1e1bb89e342c6afd0fd4c
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-01-21 20:16:07 +0000

    added code to scores.js file for project

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,8 +1,22 @@
 // Create Functions to execute printing and clearing highscores
 
 function printHighScores(){
+    let = highScores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+    highScores.sort(function(a, b) {
+        return b.score - a.score;
+    })
+
+    highScores.forEach(function(score) {
+        let li = document.createElement('li');
+        li.textContent = `${score.initials} - ${score.score}`
+
+        let ol = document.getElementById('highscores');
+        ol.appendChild(li);
+    })
 
 }
+
 
 function clearHighScores(){
 
